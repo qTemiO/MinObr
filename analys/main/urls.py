f94e835e18d007c9e1bc56d6efc276ef4c767340
qTemiO <zhitihin.artem1441@yandex.ru>
2021-04-17 09:37:18 +0300

    New Views
    added def_prof views, + procents of vipusk/vacansy

--- analys/main/urls.py
+++ analys/main/urls.py
@@ -2,10 +2,18 @@ from django.urls import path
 
 from .views import (
     VacancyHeatMap,
+    VacancyProcentHeatMap,
     VipuskHeatMap,
+    VipuskProcentHeatMap,
+
+    ProficitDeficitHeatMap,
 )
 
 urlpatterns = [
     path('heat/vacansy/', VacancyHeatMap.as_view()),
+    path('heat/vacansyproc/', VacancyProcentHeatMap.as_view()),
     path('heat/vipusk/', VipuskHeatMap.as_view()),
+    path('heat/vipuskproc/', VipuskProcentHeatMap.as_view()),
+
+    path('heat/tendency/', ProficitDeficitHeatMap.as_view()),
 ]
